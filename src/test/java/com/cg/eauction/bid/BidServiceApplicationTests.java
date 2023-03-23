746806d5a84bb67189b2e26456de4d68e9c8ebb1
avi3s <avirup.pal@gmail.com>
2023-03-23 19:57:57 +0530

    first commit

---
+++ src/test/java/com/cg/eauction/bid/BidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.eauction.bid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
